8d53f71e36f97d2128bfe9f7b09b9a70df1aa695
Felix Terkhorn <>
2018-06-10 15:41:28 -0400

    row col

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ mod pixel_math;
 use pixel_math::BOARD_SIZE;
 
 fn main() {
-    let it_something = BOARD_SIZE;
-    println!("{}", it_something);
-    println!("Hello, world!");
+    println!("board size is {}", BOARD_SIZE);
+    
+    let rows = pixel_math::rows();
+    for (i,r) in rows.iter().enumerate() {
+        println!("row {} {}", i, r);
+    }
+
+    let columns = pixel_math::columns();
+    for (j,c) in columns.iter().enumerate() {
+        println!("col {} {}", j, c);
+    }
 }
